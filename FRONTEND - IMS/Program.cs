f6db3d13f0e2e16c1ed035644ecc4f6cc714b6fe
Elke Boonen <u0116591@thomasmore.be>
2022-10-25 15:47:09 +0200

    IMS 25/10

--- FRONTEND - IMS/Program.cs
+++ FRONTEND - IMS/Program.cs
@@ -1,23 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.Json;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using RestSharp;
-
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace FRONTEND___IMS
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var client = new RestClient();
             var request = new RestRequest(@"https://localhost:5001/Songs", Method.Get);
-            var response = client.Execute(request);
+
+            var response = await client.ExecuteAsync(request);
 
             Console.WriteLine(response.Content);
 
             List<Song> songs = JsonSerializer.Deserialize<List<Song>>(response.Content);
             Console.WriteLine(String.Join("\n",songs));
+
+            /*foreach (var item in songs)
+            {
+                Console.WriteLine(item);
+            }*/
 
             Console.WriteLine("Kies een nummer: ");
             int songId = Convert.ToInt32(Console.ReadLine());
@@ -26,7 +35,7 @@ namespace FRONTEND___IMS
 
 
             request = new RestRequest(@"https://localhost:5001/Artists/" + song.artist, Method.Get);
-            response = client.Execute(request);
+            response = await client.ExecuteAsync(request);
 
             if (response.StatusCode == System.Net.HttpStatusCode.OK)
             {
@@ -36,23 +45,23 @@ namespace FRONTEND___IMS
                 string track = Uri.EscapeUriString(song.title + " " + artist.name);
 
                 request = new RestRequest(url+track, Method.Get);
-                request.AddHeader("X-RapidAPI-Key", "");
+                request.AddHeader("X-RapidAPI-Key", "API-KEY");
                 request.AddHeader("X-RapidAPI-Host", "spotify-scraper.p.rapidapi.com");
-                response = client.Execute(request);
+                response = await client.ExecuteAsync(request);
 
-                Console.WriteLine(response.Content);
+                dynamic data = JObject.Parse(response.Content);
+                Console.WriteLine(data.spotifyTrack.shareUrl);
 
+                song.spotify = data.spotifyTrack.shareUrl;
+                request = new RestRequest(@"https://localhost:5001/Songs?id=" + song.id, Method.Put);
+                request.AddStringBody(JsonConvert.SerializeObject(song), "application/json");
+                response = await client.ExecuteAsync(request);
+                Console.WriteLine(response.StatusCode + " " + response.Content);
 
             }
             else Console.WriteLine(response.Content.ToString());
 
 
-         
-
-            /*foreach (var item in songs)
-            {
-                Console.WriteLine(item);
-            }*/
         }
     }
 }
