ab4afe714fb5df296ba38b8c6fcd65a58ca36096
Elke Boonen <u0116591@thomasmore.be>
2023-03-24 12:10:38 +0100

    les 6

---
+++ Call Elvis - IMS/Artist.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Call_Elvis___IMS
+{
+    internal class Artist
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public override string ToString()
+        {
+            return $"{id} - {name}";
+        }
+    }
+}
--- Call Elvis - IMS/Program.cs
+++ Call Elvis - IMS/Program.cs
@@ -1,4 +1,5 @@
-﻿using RestSharp;
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
 using System.Text.Json;
 
 namespace Call_Elvis___IMS
@@ -8,16 +9,43 @@ namespace Call_Elvis___IMS
         static void Main(string[] args)
         {
             RestClient client = new RestClient();
+            
+            //songs eruit halen
             RestRequest request = new RestRequest("http://webservies.be/eurosong/Songs", Method.Get);
             RestResponse response = client.Execute(request);
-            Console.WriteLine(response.Content);
-            Console.WriteLine(response.StatusCode);
-
+            //Console.WriteLine(response.Content);
+            //Console.WriteLine(response.StatusCode);
             List<Song> songs = JsonSerializer.Deserialize<List<Song>>(response.Content);
-            foreach (var item in songs)
+
+            //artists eruit halen
+            request = new RestRequest("http://webservies.be/eurosong/Artists", Method.Get);
+            response = client.Execute(request);
+            List<Artist> artists = JsonSerializer.Deserialize<List<Artist>>(response.Content);
+
+            string artist = "";
+
+
+            foreach (Song item in songs)
             {
-                Console.WriteLine(item);
+                artist = artists.Find(x => x.id == item.artist).name;
+                Console.WriteLine($"{item.title} wordt gezongen door {artist}");
             }
+
+            Song song = songs.Find(x => x.title == "Hello");
+            artist = artists.Find(x => x.id == song.artist).name;
+
+            string url = $"https://spotify-scraper.p.rapidapi.com/v1/track/download/soundcloud?track={song.title}%20{artist}";
+            request = new RestRequest(url, Method.Get);
+            request.AddHeader("X-RapidAPI-Key", "key");
+            request.AddHeader("X-RapidAPI-Host", "spotify-scraper.p.rapidapi.com");
+            response = client.Execute(request);
+            Console.WriteLine(response.Content);
+            //gebruik https://codebeautify.org/jsonviewer om json code leesbaarder te maken
+
+            dynamic data = JObject.Parse(response.Content);
+            Console.WriteLine(data.spotifyTrack.shareUrl);
+
+
         }
     }
 }
