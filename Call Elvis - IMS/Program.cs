ccfbb11d5652a3514697e2f67559d6491a8a2609
Elke Boonen <u0116591@thomasmore.be>
2023-03-24 11:24:54 +0100

    les 6

---
+++ Call Elvis - IMS/Program.cs
@@ -0,0 +1,23 @@
+﻿using RestSharp;
+using System.Text.Json;
+
+namespace Call_Elvis___IMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RestClient client = new RestClient();
+            RestRequest request = new RestRequest("http://webservies.be/eurosong/Songs", Method.Get);
+            RestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+            Console.WriteLine(response.StatusCode);
+
+            List<Song> songs = JsonSerializer.Deserialize<List<Song>>(response.Content);
+            foreach (var item in songs)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Call Elvis - IMS/Song.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Call_Elvis___IMS
+{
+    internal class Song
+    {
+        /*  "id": 5,
+            "title": "Rebirth",
+            "artist": 11,
+            "spotify": "string"*/
+
+        public int id { get; set; }
+        public string title { get; set; }
+        public int artist { get; set; }
+        public string spotify { get; set; }
+
+        public override string ToString()
+        {
+            return $"{id} - {title} - {artist} - {spotify}";
+        }
+    }
+}
